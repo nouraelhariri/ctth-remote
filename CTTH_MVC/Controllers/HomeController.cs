2a3680705c9e5992e43c41b4873fe68de42a3137
unknown <nouraelhariri09@gmail.comgit config --global user.email nouraelhariri09@gmail.comgit config --global user.email nouraelhariri09@gmail.com>
2020-09-08 16:48:19 +0200

    fichier and video

--- CTTH_MVC/Controllers/HomeController.cs
+++ CTTH_MVC/Controllers/HomeController.cs
@@ -5,14 +5,31 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
+
+
+using System.Web;
+using System.Net;
+
+
+using System.IO;
+using System.Net;
+
+using System.Data.SqlClient;
 namespace Projet1_ASP.Controllers
 {
     public class HomeController : Controller
     {
         List<FileResult> files ;
+       string fichier;
 
 
         public ActionResult Index()
@@ -43,29 +60,41 @@ namespace Projet1_ASP.Controllers
 
 
         [HttpPost]
-        public ActionResult Index(User_mail m)
+        public ActionResult Index(User_mail m, HttpPostedFileBase file)
         {
 
-            User_mail a = new User_mail(Request.Form["m"], Request.Form["p"],Request.Form["subject"],Request.Form["message"]);
+
+           
+            String ser = Server.MapPath("");
+            String fileName = file.FileName;
+
+            // Append the name of the file to upload to the path.
+            ser += "\\"+fileName;
+            file.SaveAs(ser);
+            
+           fichier = ser;            User_mail a = new User_mail(Request.Form["m"], Request.Form["p"],Request.Form["subject"],Request.Form["message"]);
 
             SMTP(a.email, "nouraelhariri09@gmail.com",a.password,  a.message,a.subject);
-            Console.WriteLine(a);
-            return View("About",a);
+
+            //file suppression
+ //System.IO.File.Delete(fichier);
+
+            return View("About");
         }
 
      
 
         
-            public ActionResult Presentation()
+            public ActionResult Presentation(string fichier )
         {
+           
 
-
-            return View();
+            return View("About");
         }
         public ActionResult Espace_telechargement()
         {
             files = new List<FileResult>();
-            string pdfFilePath = "C:/Users/PC/Desktop/stage/Projet_Archivage/Projet1_ASP/Projet1_ASP/pdf/Rapport.pdf";
+            string pdfFilePath = Server.MapPath("..") +"/963852.pdf";
             byte[] bytes = System.IO.File.ReadAllBytes(pdfFilePath);
 
             files.Add(GetReport());
@@ -78,7 +107,7 @@ namespace Projet1_ASP.Controllers
 
         public FileResult GetReport()
         {
-            string ReportURL = "C:/Users/PC/Desktop/stage/Projet_Archivage/Projet1_ASP/Projet1_ASP/pdf/Rapport.pdf";
+            string ReportURL = "C:/Users/DELL/Desktop/git_ctth/ctth-remote/CTTH_MVC/963852.pdf";
             byte[] FileBytes = System.IO.File.ReadAllBytes(ReportURL);
             return File(FileBytes, "application/pdf");
         }
@@ -86,7 +115,7 @@ namespace Projet1_ASP.Controllers
         {
 
             
-            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            byte[] bytes = System.IO.File.ReadAllBytes(Server.MapPath("..") +path);
             //If file exists....
 
             MemoryStream ms = new MemoryStream(bytes, 0, 0, true, true);
@@ -110,7 +139,7 @@ namespace Projet1_ASP.Controllers
 
             return View();
         }
-        public ActionResult Accreditations()
+        public ActionResult Pourquoi_CTTH()
         {
 
 
@@ -163,8 +192,19 @@ namespace Projet1_ASP.Controllers
             // smtp.Port = 465;
             smtp.Host = "smtp.gmail.com";
             smtp.EnableSsl = true;
-            smtp.Send(email);
+           attachement(email, fichier);
             
+          
+            smtp.Send(email);
+          
+        }
+      public Attachment attachement(MailMessage email ,string fichier )
+        {
+            Attachment data = new Attachment(fichier, MediaTypeNames.Application.Octet);
+
+email.Attachments.Add(data);
+          
+            return data;
         }
     }
 
